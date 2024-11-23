826281a9a1927b2efd28f511fcebc0c537ef746c
Gurpreet kaur <gromana2022@gmail.com>
2024-11-22 22:43:15 -0600

    First commit

---
+++ assets/js/index.js
@@ -0,0 +1,33 @@
+'use strict';
+
+function select(selector, scope = document) {
+    return scope.querySelector(selector);
+}
+
+function listen(event, selector, callback) {
+    return selector.addEventListener(event, callback);
+}
+
+const cardOfImage = select('.image-card');
+const headingText = select('.heading');
+const btn = select('.btn-1');
+
+// This does not work. 
+listen (btn, 'mouseover', () => {
+    btn.classList.add('hovered');
+});
+
+listen(btn, 'mouseout', () => {
+    btn.classList.remove('hovered');
+});
+
+// I google this getboundClientReact() 
+listen(window, 'scroll', () => {
+    cardOfImage.forEach(card => {
+        const cardPosition = card.getBoundingClientRect().top;
+        const windowHeight = window.innerHeight;
+        if (cardPosition >= 0 && cardPosition.bottom <= windowHeight) {
+            card.classList.add('visible');
+        }
+    });
+});
